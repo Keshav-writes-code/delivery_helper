1194d8d80cebd79cdd39b43bfab069ed3802a96e
RidhamSood <ridhamsood1410@gmail.com>
2025-07-18 16:48:52 +0530

    register user_table

--- google_map_project/google_map_app/admin.py
+++ google_map_project/google_map_app/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 #google_map_app/models
 
-from .models import Student
+from .models import Student,user_table
 admin.site.register(Student)
+admin.site.register(user_table)
 
