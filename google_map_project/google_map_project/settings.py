6a841e05b053b10a2d307ffa4005f6088eb1f54e
Ridham <142566444+RidhamSood@users.noreply.github.com>
2025-07-18 19:37:45 +0530

    Update settings.py

--- google_map_project/google_map_project/settings.py
+++ google_map_project/google_map_project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'delivery_management_system',
         'USER': 'root',
-        'PASSWORD': 'Ridh@m2346',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
