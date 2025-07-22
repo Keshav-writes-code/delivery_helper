b8edafeefbb177bc2634fdc24fdd87ae8916af74
RidhamSood <ridhamsood1410@gmail.com>
2025-07-22 16:43:42 +0530

    do some checks

--- google_map_project/google_map_app/models.py
+++ google_map_project/google_map_app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class student(models.Model):
     student_change = models.IntegerField(null=True)
     student_id = models.BooleanField(blank=True)
+    student_charge = models.IntegerField(blank=True)
 class UserManager(BaseUserManager):
     def create_user(self, user_id, email_id, password=None, **extra_fields):
         if not user_id:
