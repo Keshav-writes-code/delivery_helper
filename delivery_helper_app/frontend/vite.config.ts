20637a6f259bcfe1bc4f2395513e92485602e115
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-07-24 20:46:15 +0530

    feat(deps): add unocss for atomic css classes

--- delivery_helper_app/frontend/vite.config.ts
+++ delivery_helper_app/frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import Unocss from "unocss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), Unocss()],
   base: "/static/",
   build: {
     outDir: "../../static/",
