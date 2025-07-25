75226fe9ec4d51566adff576a6ccefb6a4725ba5
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-07-25 16:42:10 +0530

    feat(routes): initialize routes for api endpoints

--- delivery_helper_app/urls.py
+++ delivery_helper_app/urls.py
@@ -1,4 +1,12 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "/api/get_delivery_location",
+        views.get_delivery_location,
+    ),
+    path("/api/get_customer_locations", views.get_customer_locations),
+    path("/api/delete_customer_locations", views.delete_customer_locations),
+    path("/api/add_or_modify_location", views.add_or_modify_location),
+]
--- delivery_helper_app/views.py
+++ delivery_helper_app/views.py
@@ -2,3 +2,17 @@ from django.shortcuts import render
 
 
 # Create your views here.
+def get_delivery_location(request):
+    return
+
+
+def get_customer_locations(request):
+    return
+
+
+def delete_customer_locations(request):
+    return
+
+
+def add_or_modify_location(request):
+    return
