1c821b1325975ca04b616fa236f7415611f1f187
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-07-25 22:54:48 +0530

    fix(routes): remove extra slash / infront of "/api/get_delivery_location"

--- delivery_helper_app/urls.py
+++ delivery_helper_app/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/api/get_delivery_location",views.get_delivery_location),
-    path("api/customer_order_location",views.get_customer_locations),
-    path("api/delete_customer_locations",views.delete_customer_locations),
-    path("api/add_modify_locations",views.add_or_modify_location),
+    path("api/get_delivery_location", views.get_delivery_location),
+    path("api/customer_order_location", views.get_customer_locations),
+    path("api/delete_customer_locations", views.delete_customer_locations),
+    path("api/add_modify_locations", views.add_or_modify_location),
     path("api/show_orders/", views.show_orders),
 ]
