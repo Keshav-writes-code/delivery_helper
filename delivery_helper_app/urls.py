3e229a459e65ad0c77f45725274e933c2e43d313
RidhamSood <ridhamsood1410@gmail.com>
2025-07-25 19:43:40 +0530

    do some little changes

--- delivery_helper_app/urls.py
+++ delivery_helper_app/urls.py
@@ -2,10 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(
-        "/api/get_delivery_location",
-        views.get_delivery_location,
-    ),
+    path("/api/get_delivery_location",views.get_delivery_location),
     path("show_orders/", views.show_orders),
     path("api/customer_order_location",views.get_customer_locations)
 
