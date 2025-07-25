bf32f3670319244bf9707dcffd8d4df7b627feae
Ashwanisaini-ux <sainisaabmokha4567@gmail.com>
2025-07-25 22:58:28 +0530

    Update views.py
    cleaning views

--- delivery_helper_app/views.py
+++ delivery_helper_app/views.py
@@ -16,32 +16,6 @@ def get_customer_locations(request):
         return HttpResponse("currently you have not added any location")
     return JsonResponse(list(customer_locations),safe=False)
 
-
-# @csrf_exempt
-# def delete_customer_locations(request):
-#     if request.method == "POST":
-#         try:
-#             data = json.loads(request.body.decode("utf-8"))
-#             owner_id = 1
-#             locations = data.get("locations", [])
-
-#             if not owner_id or not locations:
-#                 return JsonResponse({"error": "Missing owner_id or locations"}, status=400)
-
-#             deleted_count, _ = location.objects.filter(owner_id=owner_id, location_name__in=locations).delete()
-
-#             if deleted_count == 0:
-#                 return JsonResponse({"message": "No matching locations found."}, status=404)
-#             else:
-#                 return JsonResponse({"message": f"{deleted_count} locations deleted successfully."})
-        
-#         except json.JSONDecodeError:
-#             return JsonResponse({"error": "Invalid JSON data"}, status=400)
-#         except Exception as e:
-#             return JsonResponse({"error": str(e)}, status=500)
-
-#     return JsonResponse({"error": "Only POST method is allowed"},status=405)
-
 @csrf_exempt
 def delete_customer_locations(request):
     if request.method == "POST":
