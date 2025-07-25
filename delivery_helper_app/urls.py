c14e27af5cdc795e1c2e4be509b2179e1258ce34
AshwaniSaini <sainisaabmokha4567@gmail.com>
2025-07-25 20:30:45 +0530

    adding api to delete given locations

--- delivery_helper_app/urls.py
+++ delivery_helper_app/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path("/api/get_delivery_location",views.get_delivery_location),
-    path("show_orders/", views.show_orders),
-    path("api/customer_order_location",views.get_customer_locations)
+    path("api/show_orders/", views.show_orders),
+    path("api/customer_order_location",views.get_customer_locations),
+    path("api/delete_customer_locations",views.delete_customer_locations),
 
 ]
