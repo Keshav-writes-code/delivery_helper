6d2c26f02a9d9d0187daaf9a58400b584febeae9
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-07-26 14:19:47 +0530

    refac(route): change name to be more clear

--- delivery_helper_app/urls.py
+++ delivery_helper_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/customer_order_location", views.get_customer_locations),
+    path("api/get_customer_locations", views.get_customer_locations),
     path("api/delete_customer_locations", views.delete_customer_locations),
     path("api/add_modify_locations", views.add_or_modify_location),
     path("api/get_delivery_agent_orders/", views.get_delivery_agent_orders),
