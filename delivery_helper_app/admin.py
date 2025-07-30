e5c78c5dbdf9aba780ddc16c27e8f82ffa2335a4
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-07-30 22:09:11 +0530

    refac: remove unsed UserManage class import

--- delivery_helper_app/admin.py
+++ delivery_helper_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import user_types, Profile, customer_order, UserManager
+from .models import user_types, Profile, customer_order
 
 # Register your models here.
 admin.site.register(user_types)
